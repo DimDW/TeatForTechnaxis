e0c3d2a736590d020f7e759f3a81008a7111dc4c
DimDW <spampr@mail.ru>
2021-05-28 22:51:35 +0300

    Initial commit

---
+++ src/main/java/dev/dim/listofbooks/ListOfBooksApplication.java
@@ -0,0 +1,13 @@
+package dev.dim.listofbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListOfBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListOfBooksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/dim/listofbooks/ListOfBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dim.listofbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListOfBooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
