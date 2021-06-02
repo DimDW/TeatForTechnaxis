385ee09e26d62616e1ef374b33d93667d3cb29a6
DmitryDW <dwronon@gmail.com>
2021-06-02 16:23:12 +0300

    AwsS3 Client&Controller

---
+++ src/main/java/dev/dim/listofbooks/controller/AwsS3Controller.java
@@ -0,0 +1,29 @@
+package dev.dim.listofbooks.controller;
+
+
+import dev.dim.listofbooks.service.AwsS3Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/storage/")
+class BucketController {
+
+    private AwsS3Client awsS3Client;
+
+    @Autowired
+    BucketController(AwsS3Client awsS3Client) {
+        this.awsS3Client = awsS3Client;
+    }
+
+    @PostMapping("/uploadFile")
+    public String uploadFile(@RequestPart(value = "file") MultipartFile file) {
+        return this.awsS3Client.uploadFile(file);
+    }
+
+    @DeleteMapping("/deleteFile")
+    public String deleteFile(@RequestPart(value = "url") String fileUrl) {
+        return this.awsS3Client.deleteFileFromS3Bucket(fileUrl);
+    }
+}
